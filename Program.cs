549a775ee7e97dca633c22e681e88dd8da4f879a
Jasper King S. Gueco <jasperkinggueco@gmail.com>
2025-09-21 14:29:21 +0800

    Add: Description about the Program

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("This program performs sorting in descending order.");
         Console.Write("How many numbers do you want to sort? ");
         int n = int.Parse(Console.ReadLine());
         int[] numbers = new int[n];
