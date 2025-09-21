a0d2e971ead346ab46e665ff0c4767c87fe3f30e
Jasper King S. Gueco <jasperkinggueco@gmail.com>
2025-09-21 14:19:09 +0800

    feat: Main Branch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        String name = "Jasper King S. Gueco";
+        String block = "BSCS 3-3";
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Section: " + block);
+        Console.WriteLine("This Repository is created for my Course Activities!");
+    }
+}
